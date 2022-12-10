ebc7d3f55a2f6cf50ba5bf14d6f3ce17db13c9ce
Miken37 <mike-166@hotmail.com>
2022-12-10 12:04:07 -0500

    Menu designed

--- main.js
+++ main.js
@@ -11,16 +11,31 @@ log("Connected");
 function menuDOM(){
     const body = document.querySelector("body")
     const container = document.createElement('form');        //Creates form container
-    container.classList = "menu-container";             //Gives container class: menu-container
+    container.classList.add("menu-container");             //Gives container class: menu-container
     const titleText = document.createElement("h1");         
-    
+    const startGameBtn = document.createElement('input')
+    startGameBtn.classList.add('start-button');
+    const playerNameOne = document.createElement('input');
+    playerNameOne.classList.add('inputs');
+    const playerNameTwo = document.createElement('input');
+    playerNameTwo.classList.add('inputs');
 
     //Giving DOM Elements HTML content
     titleText.innerHTML = "Tic-Tac-Toe";
 
+    startGameBtn.setAttribute('type', 'submit');
+    startGameBtn.setAttribute('value', 'PLAY'); 
+
+    playerNameOne.setAttribute('placeholder', 'Player 1');
+    playerNameOne.setAttribute('name', 'playerOneName');
+    playerNameTwo.setAttribute('placeholder', 'Player 2');
+    playerNameTwo.setAttribute('name', 'playerTwoName');
+
     //Appending elements to body/container
     body.appendChild(container);                //Adds form to body
     container.appendChild(titleText);
-    
+    container.appendChild(startGameBtn);
+    container.appendChild(playerNameOne);
+    container.appendChild(playerNameTwo);
 }
 
