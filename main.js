babbdf299c3665359fae98c9d98465a237fc495e
Miken37 <mike-166@hotmail.com>
2022-12-21 07:05:14 -0500

    Working on controller to alternate player turns

--- main.js
+++ main.js
@@ -48,12 +48,14 @@ function menuDOM(){
     });
 }
 
-function startGame(playerOne, playerTwo){
-    log("started game");
-    log(`Player One is ${playerOne.name} and is ${playerOne.mark}`);
-    log(`Player Two is ${playerTwo.name} and is ${playerTwo.mark}`);
-    drawBoard();
-    log(drawBoard().cells)
+function startGame(playerOne, playerTwo){       //Draws board and makes array for cells. Puts it into gameController
+    let cells = drawBoard();            //cells is an array containing each square on the board
+    cells.forEach(e => {
+        e.addEventListener('click', function(){
+            gameController(e.id, playerOne, playerTwo);
+        });
+    }); 
+    
 }
 
 function playerData(playerName, markIndex){
@@ -64,9 +66,31 @@ function playerData(playerName, markIndex){
 };
 
 function drawBoard(){
-    const boardContainer = document.createElement('div');
-    boardContainer.classList.add("game-container");
-    document.querySelector('body').appendChild(boardContainer); //appends to body
+    const mainContainer = document.createElement('div');        //Houses board, player info and win screen
+    mainContainer.classList.add('main-container');              //Gives class
+    document.querySelector('body').appendChild(mainContainer); //appends to body
+
+    const headContainer = document.createElement('div');        //Contains player info per turn
+    headContainer.classList.add('head-container');      
+    mainContainer.appendChild(headContainer);
+
+    const playerOneHeader = document.createElement('div');      
+    headContainer.appendChild(playerOneHeader);
+    playerOneHeader.classList.add('player-one-header')
+    playerOneHeader.innerHTML = 'p1 name';
+
+    const emptyDiv = document.createElement('div');
+    headContainer.appendChild(emptyDiv);
+    emptyDiv.classList.add('empty');
+
+    const playerTwoHeader = document.createElement('div');
+    headContainer.appendChild(playerTwoHeader);
+    playerTwoHeader.classList.add('player-two-header')
+    playerTwoHeader.innerHTML = 'p2 name';
+
+    const boardContainer = document.createElement('div');       //Creates board grid
+    boardContainer.classList.add("game-container"); 
+    mainContainer.appendChild(boardContainer);                  //Assigns to be within mainContainer
     let cells = [];   //Empty array for the squares
     for (let i = 0; i < 9; i++){
         const square = document.createElement('div');
@@ -75,5 +99,45 @@ function drawBoard(){
         boardContainer.appendChild(square);
         cells.push(square);         //Adds each square to the empty array
     }
-    return {cells};
+    
+    return cells;
+}
+
+function gameController(cell, playerOne, playerTwo){
+    log(`Clicked: ${cell}`);
+    let cellEle = document.querySelector(`#${cell}`);
+    const p1 = playerOne.name;
+    const p2 = playerTwo.name;
+    let currentPlayer = findCurrentPlayer(playerOne.name, playerTwo.name); 
+    log(currentPlayer);
+    // if (currentPlayer == p1){
+    //     cellEle.style.backgroundColor = "red";
+    //     currentPlayer = p2;
+    //     return currentPlayer;
+    // } else{
+    //     cellEle.style.backgroundColor = "blue";
+    //     currentPlayer = p1;
+    //     return currentPlayer;
+    // }
+    
+}
+
+
+function findCurrentPlayer(playeroneName, playertwoName){
+    const playerTwo = document.querySelector('.player-two-header');
+    const playerOne = document.querySelector('.player-one-header');
+    if (playerOne.style.color == "cyan"){
+        playerOne.style.color = "black";
+        playerOne.style.fontSize = "20px";
+        let currentPlayer = playerTwo.innerHTML;
+        playerTwo.style.color = "cyan";
+        playerTwo.style.fontSize = "35px";
+        return currentPlayer;
+    }
+    let currentPlayer = playerOne.innerHTML;
+    playerTwo.style.color = "black";
+    playerTwo.style.fontSize = "20px";
+    playerOne.style.color = "cyan";
+    playerOne.style.fontSize = "35px";
+    return currentPlayer;
 }
